f19d8a4bc5192d853c59af3399a784aca7515e0a
JUAN ANDRÉS CÁRDENAS DIAZ <juancadi@MacBook-Pro-de-JUAN.local>
2015-12-10 23:56:35 -0500

    Se sincroniza playground asociado al mini reto de la semana 2

---
+++ semana-2/MiniReto-Semana2.playground/Contents.swift
@@ -0,0 +1,32 @@
+//Mini Reto Semana 2 - Swift
+
+import UIKit
+
+let  range = 0...100
+
+for num in range{
+
+    if(num % 5 == 0){
+    
+        print("#\(num) Bingo!!!")
+    
+    }
+    
+    if(num % 2 == 0){
+    
+        print("#\(num) Par!!!")
+    
+    }
+    
+    if(num % 2 != 0){
+        
+        print("#\(num) Impar!!!")
+    }
+    
+    if (num >= 30 && num <= 40){
+    
+        print("#\(num) VivaSwift!!!")
+    
+    }
+    
+}
